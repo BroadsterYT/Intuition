79145010645a133197c82b8abf6b12860eb19473
BroadsterYT <80648000+BroadsterYT@users.noreply.github.com>
2025-09-27 18:01:49 -0400

    Further generalized InlineValueEntry

--- Source/InlineValueEntry.h
+++ Source/InlineValueEntry.h
@@ -14,9 +14,24 @@
 
 class InlineValueEntry : public juce::Component {
 public:
-    InlineValueEntry(double currentValue);
+    InlineValueEntry(double currentValue) {
+        addAndMakeVisible(editor);
 
-    void setValueChangeCallback(std::function<void(double)> callback);
+        editor.setText(juce::String(currentValue));
+        editor.setSelectAllWhenFocused(true);
+        editor.setInputRestrictions(0, "0123456789.-"); // only numbers
+        editor.setJustification(juce::Justification::centred);
+        editor.setColour(juce::TextEditor::backgroundColourId, juce::Colours::black);
+        editor.setColour(juce::TextEditor::outlineColourId, juce::Colours::grey);
+        editor.setColour(juce::TextEditor::textColourId, juce::Colours::white);
+
+        setExitKeyLambdas();
+    }
+
+    void setValueChangeCallback(std::function<void(double)> callback) {
+        valueChangeCallback = callback;
+        setExitKeyLambdas();
+    }
 
     template<typename ComponentType>
     void linkToComponent(ComponentType* component, std::function<void(ComponentType*, double)> setter) {
@@ -27,11 +42,28 @@ public:
         }
     }
 
-    void resized() override;
+    void resized() override {
+        editor.setBounds(getLocalBounds().reduced(4));
+    }
 
 private:
     juce::TextEditor editor;
     std::function<void(double)> valueChangeCallback;
 
-    void setExitKeyLambdas();
+    void setExitKeyLambdas() {
+        editor.onReturnKey = [this] {
+            if (valueChangeCallback) {
+                valueChangeCallback(editor.getText().getDoubleValue());
+            }
+            if (auto* cb = findParentComponentOfClass<juce::CallOutBox>()) {
+                cb->exitModalState(1);  // Close popup
+            };
+        };
+
+        editor.onEscapeKey = [this] {
+            if (auto* cb = findParentComponentOfClass<juce::CallOutBox>()) {
+                cb->exitModalState(0);  // Cancel
+            };
+        };
+    }
 };
